ee120cfbc1f856c4450a165cd3259ac1a36b60d4
yoonsuk <yoonsuk.choi93@gmail.com>
2023-11-21 20:10:22 +0900

    AWS, PuTTY 학습

---
+++ Python/231120_AWS.py
@@ -0,0 +1,30 @@
+a = ['AWS를 배워보자']
+print(a)
+# AWS
+# AWS는 클라우드 가상환경이다.
+
+# EC2 서비스의 사용
+# 클라우드 안에 나만의 가상 서버를 만드는 것
+
+# 인스턴스
+# 이걸 만든다. 여러 이론이나 정의가 있지만, 작은 컴퓨터라고 생각하라.
+# 메모리나 성능이 좋으면 돈이 든다.
+# 새로가입하면 1년은 무료~(전부 다는 아님)
+
+# 다 같이 다수가 사용하면 생성되는 '인스턴트 패키지'를 전달해야 한다.
+
+
+b = ['PuTTY를 배워보자']
+print(b)
+# PuTTY는 터미널(검은 화면) 관리 해주는 에뮬레이터
+# 컴퓨터 안에 작은 컴퓨터 개념
+# 옛날 게임 돌아가는 것도 가능하다!
+
+c = ['PuTTY의 단계적 접속']
+print(c) 
+# PuTTY Configuration접속 > Connection > SSH > Auth > Credentials
+# Credentials 화면 > Private key file for authentication 여기에 인스턴스 주소 넣자.
+
+d = ['PuTTY의 사용']
+print(d) 
+# 관리자 로그인 아이디는 ubuntu
