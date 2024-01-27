32bbe4fcf0167050a39dec4d23b37e588fcd4de7
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2024-01-27 23:43:56 +0900

    Update 240125_빨간네모 피하기 게임.py

--- Pygame/240125_빨간네모 피하기 게임.py
+++ Pygame/240125_빨간네모 피하기 게임.py
@@ -17,6 +17,7 @@ red = (255, 0, 0)
 pygame.font.init()
 font = pygame.font.SysFont("comicsans", 40)
 
+#플레이어 설정과 떨어지는 물체의 설정 값이 다르면, 타겟에 맞은 효과가 안 뜸,...
 # 플레이어 설정
 player_size = 50
 player_pos = [width / 2, height - 2 * player_size]
@@ -26,6 +27,7 @@ player_speed = 10
 enemy_size = 50
 enemy_pos = [random.randint(0, width - enemy_size), 0]
 enemy_speed = 10
+
 
 clock = pygame.time.Clock()
 
