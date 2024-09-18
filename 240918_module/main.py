58f5d5a71a0143d4c9fff2e2fe27598eb522951e
YoonSukChoi <yoonsuk.choi93@gmail.com>
2024-09-18 23:26:29 +0900

    module2

--- 240918_module/main.py
+++ 240918_module/main.py
@@ -1,9 +1,9 @@
 import my_module
 
 # greet 함수 호출
-message = my_module.greet("철수")
+message = my_module.greet("YoonSuk")
 print(message)
 
 # add 함수 호출
-result = my_module.add(3, 5)
-print(f"3과 5를 더한 결과는: {result}")
+result = my_module.add(7, 5)
+print(f"7과 5를 곱한 결과는: {result}")
--- 240918_module/my_module.py
+++ 240918_module/my_module.py
@@ -1,7 +1,7 @@
 def greet(name):
     """주어진 이름으로 인사하는 함수"""
-    return f"안녕하세요, {name}님!"
+    return f"Hi, {name}!"
 
 def add(a, b):
-    """두 수를 더하는 함수"""
-    return a + b
+    """두 수를 곱하는 함수"""
+    return a * b
