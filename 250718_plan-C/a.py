d12d9ecf454fa9be343189e932b792fbfe440c79
YoonSukChoi <yoonsuk.choi93@gmail.com>
2025-07-18 19:01:15 +0900

    pygame

--- 250718_plan-C/a.py
+++ 250718_plan-C/a.py
@@ -1,2 +1,12 @@
 import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Move the Box")
+
+x, y = 250, 250
+width, height = 40, 40
+vel = 5
+
+run = True
 
