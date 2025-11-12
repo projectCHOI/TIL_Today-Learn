77e4334b475ded96d87175dceca3b35ef6d70b26
YoonSukChoi <yoonsuk.choi93@gmail.com>
2025-11-13 00:50:29 +0900

    Stage

--- 250718_plan-C/pygame-5.py
+++ 250718_plan-C/pygame-5.py
@@ -6,7 +6,7 @@ import re
 
 # === 기본 설정 ===
 pygame.init()
-WIDTH, HEIGHT = 640, 400
+WIDTH, HEIGHT = 800, 600
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("미로 탐험형 - JSON 스테이지 로더")
 
