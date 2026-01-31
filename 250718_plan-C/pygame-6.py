7f4ce2efd2ab49900dee35255cf0af75bee7daab
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-02-01 00:07:59 +0900

    mouse_pos

--- 250718_plan-C/pygame-6.py
+++ 250718_plan-C/pygame-6.py
@@ -71,12 +71,10 @@ while running:
             mouse_pos = pygame.mouse.get_pos()
             drag_vec = pygame.Vector2(mouse_pos) - pygame.Vector2(press_pos)
 
-            drag_vec = pygame.Vector2(release_pos) - pygame.Vector2(press_pos)
-
             if drag_vec.length() > MAX_DRAG:
                 drag_vec = drag_vec.normalize() * MAX_DRAG
 
-            drag_dist = drag_vec.length()
+            aim_end = pygame.Vector2(press_pos) + drag_vec
 #
             # 거리 → 단계
             level = int(drag_dist / (MAX_DRAG / MAX_LEVEL))
