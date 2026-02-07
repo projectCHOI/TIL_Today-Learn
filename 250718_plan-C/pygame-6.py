0802642b5bfc461f9dfae054ec1c0b1100374be6
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2026-02-08 00:06:50 +0900

    pygame

--- 250718_plan-C/pygame-6.py
+++ 250718_plan-C/pygame-6.py
@@ -16,3 +16,34 @@ BLUE = (0, 100, 255)  # 플레이어 색상
 RED = (255, 0, 0)    # 적 색상
 GRAY = (200, 200, 200)
 DARK_BLUE = (0, 50, 150) # 패널티 상태 플레이어
+
+# 폰트 설정
+try:
+    FONT_PATH = r"C:\Users\boss3\OneDrive\바탕 화면\GitHub\TIL_Today-Learn\Open Font License\KCC한빛체\KCC한빛체\KCC-Hanbit.ttf"
+    font = pygame.font.Font(FONT_PATH, 20)
+except:
+    font = pygame.font.SysFont("arial", 20)
+
+# 플레이어 설정
+PLAYER_SIZE = 50
+PLAYER_SPEED = 5
+player_pos = pygame.Vector2(WIDTH // 2, HEIGHT // 2)
+
+# 적(Target) 설정
+ENEMY_SIZE = 50
+ENEMY_SPEED = 3
+enemy_pos = pygame.Vector2(100, 100)
+enemy_move_timer = pygame.time.get_ticks()
+enemy_is_moving = True
+
+# 투사체 설정
+PROJECTILE_RADIUS = 6
+MAX_DRAG = 200
+MAX_LEVEL = 10
+POWER_TABLE = [0, 0, 8, 10, 12, 14, 16, 18, 20, 22, 25]
+
+# 상태 변수
+dragging = False
+press_pos = pygame.Vector2(0, 0)
+projectiles = []
+can_move = True
