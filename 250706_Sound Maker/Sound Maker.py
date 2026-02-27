7ed7b44e39438e59b5d180584f163371044f7bec
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-02-28 00:45:38 +0900

    _get_y_offset

--- 250706_Sound Maker/Sound Maker.py
+++ 250706_Sound Maker/Sound Maker.py
@@ -144,13 +144,13 @@ class MusicMaker:
             
             self.canvas.create_text(x, y_base + 90, text=note, font=("맑은 고딕", 8), tag="note")
 
-    #def _get_y_offset(self, note):
+    def _get_y_offset(self, note):
         mapping = {
-            "도": 24, "레": 16, "미": 8, "파": 0, "솔": -8, "라": -16, "시": -24
+            "도": 40, "레": 30, "미": 20, "파": 10, "솔": 0, "라": -10, "시": -20
         }
         base = mapping.get(note[0], 0)
-        if "+" in note: return base - 56
-        if "-" in note: return base + 56
+        if "+" in note: return base - 70 
+        if "-" in note: return base + 70
         return base
 
     def add_note(self, note):
