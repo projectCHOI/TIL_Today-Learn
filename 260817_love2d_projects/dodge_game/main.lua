f9af3c59877942166ae90a1da9bd6157f9fa9283
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-08-17 00:37:35 +0900

    blue

--- 260817_love2d_projects/dodge_game/main.lua
+++ 260817_love2d_projects/dodge_game/main.lua
@@ -1,7 +1,39 @@
 function love.load()
-    message = "Hello, LÖVE2D!"
+    player = {
+        x = 215,
+        y = 310,
+        width = 50,
+        height = 25,
+        speed = 300
+    }
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("left") then
+        player.x = player.x - player.speed * dt
+    end
+
+    if love.keyboard.isDown("right") then
+        player.x = player.x + player.speed * dt
+    end
+
+    if player.x < 0 then
+        player.x = 0
+    end
+
+    if player.x + player.width > 480 then
+        player.x = 480 - player.width
+    end
 end
 
 function love.draw()
-    love.graphics.print(message, 170, 170)
+    love.graphics.setColor(0.2, 0.7, 1.0)
+
+    love.graphics.rectangle(
+        "fill",
+        player.x,
+        player.y,
+        player.width,
+        player.height
+    )
 end
