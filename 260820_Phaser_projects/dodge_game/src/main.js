48971b70fa6b366e1e05d2a5d6776b420a74c898
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-08-22 00:15:00 +0900

    JS

--- 260820_Phaser_projects/dodge_game/src/main.js
+++ 260820_Phaser_projects/dodge_game/src/main.js
@@ -1,1 +1,38 @@
-"phaser"
+import Phaser from "phaser";
+
+class MainScene extends Phaser.Scene {
+    constructor() {
+        super("MainScene");
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor("#11131a");
+
+        this.add
+            .text(
+                240,
+                180,
+                "Hello, Phaser!",
+                {
+                    fontFamily: "Arial",
+                    fontSize: "24px",
+                    color: "#ffffff"
+                }
+            )
+            .setOrigin(0.5);
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+
+    width: 480,
+    height: 360,
+
+    parent: "game-container",
+    backgroundColor: "#11131a",
+
+    scene: [MainScene]
+};
+
+new Phaser.Game(config);
