3acfaaa5e5f749efd235b596339e0f6fd19659e5
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-08-22 01:03:53 +0900

    Game

--- 260820_Phaser_projects/dodge_game/src/main.js
+++ 260820_Phaser_projects/dodge_game/src/main.js
@@ -6,18 +6,42 @@ class MainScene extends Phaser.Scene {
     create() {
         this.cameras.main.setBackgroundColor("#11131a");
 
-        this.add
-            .text(
-                240,
-                180,
-                "Hello, Phaser!",
-                {
-                    fontFamily: "Arial",
-                    fontSize: "24px",
-                    color: "#ffffff"
-                }
-            )
-            .setOrigin(0.5);
+        this.player = this.add.rectangle(
+            240,
+            322.5,
+            50,
+            25,
+            0x33b3ff
+        );
+
+        this.playerSpeed = 300;
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.input.keyboard.addCapture([
+            Phaser.Input.Keyboard.KeyCodes.LEFT,
+            Phaser.Input.Keyboard.KeyCodes.RIGHT
+        ]);
+    }
+
+    update(time, delta) {
+        const dt = delta / 1000;
+
+        if (this.cursors.left.isDown) {
+            this.player.x -= this.playerSpeed * dt;
+        }
+
+        if (this.cursors.right.isDown) {
+            this.player.x += this.playerSpeed * dt;
+        }
+
+        const playerHalfWidth = this.player.width / 2;
+
+        this.player.x = Phaser.Math.Clamp(
+            this.player.x,
+            playerHalfWidth,
+            480 - playerHalfWidth
+        );
     }
 }
 
