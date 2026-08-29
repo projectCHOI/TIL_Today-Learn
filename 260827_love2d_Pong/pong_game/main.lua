fca78a2356290f9992ea8e1170d5fdfb9be6e623
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-08-29 13:30:54 +0900

    local ai

--- 260827_love2d_Pong/pong_game/main.lua
+++ 260827_love2d_Pong/pong_game/main.lua
@@ -1,6 +1,7 @@
 local WINDOW_WIDTH = 800
 local WINDOW_HEIGHT = 600
 
+-- 플레이어 패들
 local player = {
     x = 40,
     y = 250,
@@ -9,6 +10,7 @@ local player = {
     speed = 300
 }
 
+-- AI 패들
 local ai = {
     x = 745,
     y = 250,
@@ -16,24 +18,32 @@ local ai = {
     height = 100
 }
 
+-- 공
 local ball = {
     x = 390,
     y = 290,
     width = 20,
     height = 20,
 
-    -- 반사 확인을 위해 X 속도를 낮춤
     dx = 120,
     dy = 180
 }
+
+local function checkCollision(a, b)
+    return a.x < b.x + b.width
+       and b.x < a.x + a.width
+       and a.y < b.y + b.height
+       and b.y < a.y + a.height
+end
+
 
 function love.load()
     love.graphics.setBackgroundColor(0, 0, 0)
 end
 
+
 function love.update(dt)
 
-    -- 플레이어 이동
     if love.keyboard.isDown("w") then
         player.y = player.y - player.speed * dt
     end
@@ -51,31 +61,46 @@ function love.update(dt)
         player.y = WINDOW_HEIGHT - player.height
     end
 
-
-    -- 공 이동
     ball.x = ball.x + ball.dx * dt
     ball.y = ball.y + ball.dy * dt
 
-
-    -- 위쪽 벽 반사
+    -- 위쪽 벽
     if ball.y <= 0 then
         ball.y = 0
         ball.dy = -ball.dy
     end
 
-
-    -- 아래쪽 벽 반사
+    -- 아래쪽 벽
     if ball.y + ball.height >= WINDOW_HEIGHT then
         ball.y = WINDOW_HEIGHT - ball.height
         ball.dy = -ball.dy
     end
+
+    if checkCollision(ball, player) and ball.dx < 0 then
+
+        -- 공을 플레이어 패들 오른쪽으로 이동
+        ball.x = player.x + player.width
+
+        -- 좌우 방향 반전
+        ball.dx = -ball.dx
+    end
+
+    if checkCollision(ball, ai) and ball.dx > 0 then
+
+        -- 공을 AI 패들 왼쪽으로 이동
+        ball.x = ai.x - ball.width
+
+        -- 좌우 방향 반전
+        ball.dx = -ball.dx
+    end
 end
+
 
 function love.draw()
 
     love.graphics.setColor(1, 1, 1)
 
-    -- 플레이어
+    -- 플레이어 패들
     love.graphics.rectangle(
         "fill",
         player.x,
@@ -84,7 +109,7 @@ function love.draw()
         player.height
     )
 
-    -- AI
+    -- AI 패들
     love.graphics.rectangle(
         "fill",
         ai.x,
