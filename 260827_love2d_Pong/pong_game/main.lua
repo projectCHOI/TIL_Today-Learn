b39fe7b9a8c3069e8f99392f1bfcc6c35e9f93fe
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2026-09-09 14:50:16 +0900

    rallyCount

--- 260827_love2d_Pong/pong_game/main.lua
+++ 260827_love2d_Pong/pong_game/main.lua
@@ -409,20 +409,24 @@ function love.update(dt)
 
         if checkCollision(ball, opponent)
             and ball.dx > 0 then
+        
             ball.x =
                 opponent.x - ball.width
         
             local hitPosition =
                 getHitPosition(ball, opponent)
-            -- 좌우 방향 반전
+        
             ball.dx = -ball.dx
-            -- 충돌 위치에 따라 세로 속도 결정
+        
             ball.dy =
                 hitPosition * MAX_BOUNCE_DY
-            -- 너무 수평이면 최소 세로 속도 적용
+        
             applyMinimumVerticalSpeed()
-            -- 랠리가 이어질수록 속도 증가
+        
             increaseBallSpeed()
+            rallyCount =
+                rallyCount + 1
+            updateAiDifficulty()
         end
 
         if ball.x > WINDOW_WIDTH then
