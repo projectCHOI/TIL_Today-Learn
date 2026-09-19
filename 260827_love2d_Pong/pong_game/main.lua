684d0b9f0c808c79fc49a872c0f46d010ac9e490
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-09-19 21:18:03 +0900

    love

--- 260827_love2d_Pong/pong_game/main.lua
+++ 260827_love2d_Pong/pong_game/main.lua
@@ -773,13 +773,12 @@ function love.draw()
     )
 
     -- 상태 안내
-    love.graphics.setFont(fontSmall)
     love.graphics.print(
         "MODE: " .. gameMode,
         20,
         20
     )
-    
+
     love.graphics.print(
         "ESC : MENU",
         20,
